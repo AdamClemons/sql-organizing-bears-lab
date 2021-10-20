b60690f3d3b9a6d37175da8482a950cb0096f171
Adam Clemons <74882592+thisClemons@users.noreply.github.com>
2021-10-20 16:41:51 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,17 @@
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+    VALUES("Mr. Chocolate", 8, "M", "brown", "angry", "y");
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+    VALUES("Rowdy", 8, "M", "brown", "angry", "y");
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+    VALUES("Tabitha", 10, "F", "brown", "happy", "y");
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+    VALUES("Sergeant Brown", 8, "M", "brown", "angry", "y");
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+    VALUES("Melissa", 8, "F", "brown", "angry", "y");
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+    VALUES("Grinch", 3, "M", "brown", "angry", "y");
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+    VALUES("Wendy", 5, "F", "brown", "angry", "y");
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+    VALUES(NULL, 8, "M", "brown", "angry", "y");
